1eabd89841f77ecfe63e6d39c6322cbe0b0a9c5f
Akshat Jain <u7284072@anu.edu.au>
2022-10-15 10:25:53 +1100

    Sync w/ WSL

--- proxy.c
+++ proxy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
@@ -6,8 +7,19 @@
 
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36\r\n";
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("%s", user_agent_hdr);
+    // Generated Port 29722
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <port> <replacement_policy>\n", argv[0]);
+    }
+
+    int listenfd = Open_listenfd(argv[1]);
+    int tinyClientFd = Open_clientfd("localhost", argv[1]);
+    char* replacement_policy = argv[2];
+
+
+    
+    Close(tinyClientFd);
     return 0;
 }
